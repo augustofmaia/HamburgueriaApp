3f1dcdd4c9b58289e6e4ab6a49a76e517812ec07
Augusto Maia <augustofmaia@hotmail.com>
2022-12-17 20:28:11 -0300

    Models e DbContext Criados

---
+++ Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace HamburgueriaApp.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        //Propriedade de navegação e relacionamento entre categoria e lanche, de um para muitos
+        public List<Lanche> Lanches { get; set;}
+    }
+}
---
+++ Models/Lanche.cs
@@ -0,0 +1,21 @@
+﻿namespace HamburgueriaApp.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Name { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+
+
+        //Essas abaixo são propriedades de navegação, não serão criadas na tabela. Usadas para definir o relacionamento em lanche e categoria
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
