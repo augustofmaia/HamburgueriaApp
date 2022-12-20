e6e9c034e28d42cb7fa9be0bec8d240d15728303
Augusto Maia <augustofmaia@hotmail.com>
2022-12-20 10:11:02 -0300

    Aplicando atributos Data Annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,10 +1,26 @@
-﻿namespace HamburgueriaApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HamburgueriaApp.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo são 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [Display(Name ="Nome")]
         public string CategoriaNome { get; set; }
+
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo são 100 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Nome")]
         public string Descricao { get; set; }
+
 
         //Propriedade de navegação e relacionamento entre categoria e lanche, de um para muitos
         public List<Lanche> Lanches { get; set;}
